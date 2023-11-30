ae4c33688435f900d629157c74723b2f5b7d8ef7
Shubham Singh <shubh622005@gmail.com>
2023-11-30 22:56:25 +0530

    Added objects directory in `vault init`

--- src/init.rs
+++ src/init.rs
@@ -1,6 +1,6 @@
 //This will have an init function which will currently create a .vault folder in the current directory
 
-use crate::branches::create;
+// use crate::branches::create;
 use std::fs;
 use std::fs::File;
 use std::path::Path;
@@ -8,15 +8,18 @@ use std::path::Path;
 pub fn init() {
     let path: &Path = Path::new(".vault");
     let file_path: std::path::PathBuf = path.join("CurrentDir");
+    let objects: std::path::PathBuf = path.join("objects");
     let ignore_file_path: &Path = Path::new(".vaultignore");
     if path.exists() {
-        println!("This directory already is a in a vault. Cannot init! ");
+        println!("This directory already is a in a vault. Cannot init!");
     } else {
-        fs::create_dir(".vault").expect("Unable to create .vault folder");
-        create("main");
+        //@TODO - Think of better error messages
+        //@TODO - If any step fails, reverse all the previous steps...,
+        // We can do this by making a temp directory and only copy the temp dir if all the steps pass
+        fs::create_dir(path).expect("Unable to create .vault folder");
+        fs::create_dir(objects).expect("Unable to create a vault! \n Some error occured");
+        fs::File::create(file_path).expect("Unable to create a vault! \n Some error occured");
         let _ = File::create(ignore_file_path);
         println!("Vault created successfully");
-        //To see how to handle errors here
-        let _ = fs::write(file_path, "main");
     }
 }
