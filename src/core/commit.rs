1536feee5390a8e2118fb99c689ac98ff1e1823d
Shubham Singh <shubh622005@gmail.com>
2023-12-01 04:11:46 +0530

    Added `date time` to Commit struct

--- src/core/commit.rs
+++ src/core/commit.rs
@@ -1,8 +1,22 @@
+use std::io;
+use chrono::{DateTime, Utc};
 use super::types::GitObject;
+
+/* SAMPLE COMMIT 
+
+commit 257\0
+tree 41\0blob 41\0fileA.txt\0123456789abcdef0123456789abcdef01234567
+parent abcdef0123456789abcdef0123456789abcdef01
+author John Doe <john@example.com> 1638286773 -0500
+committer John Doe <john@example.com> 1638286773 -0500
+
+Commit message goes here.
+ */
+
 
 #[derive(Debug)]
 pub struct Commit {
-    //Add timestamp and Timezone
+    date: DateTime<Utc>,
     message: String,
     author: String,
     timestamp: u64,
@@ -10,3 +24,9 @@ pub struct Commit {
     parent: Option<Vec<Commit>>
 }
 
+impl Commit {
+    pub fn new_commit() -> io::Result<Self>{
+        let date = Utc::now();
+        todo!()
+    }
+}
