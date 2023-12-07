18ded58ff4025526ef424e0641f91abd578b0a49
Shubham Singh <shubh622005@gmail.com>
2023-12-07 21:46:15 +0530

    Removed unused import

--- src/hash.rs
+++ src/hash.rs
@@ -1,6 +1,6 @@
 //Docummentation - https://docs.rs/sha256/latest/sha256/
 
-use sha256::{digest, try_digest};
+use sha256::digest;
 
 pub fn hash_in_SHA256(string_to_hash: &str) -> String {
     let input_string: String = String::from(&string_to_hash.to_string());
