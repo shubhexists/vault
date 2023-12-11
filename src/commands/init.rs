8afa9cc24febad1aada963328b43bef46bfbed56
Drac <gpt.sahaj28@gmail.com>
2023-12-11 17:24:46 +0530

    Init command can now make init.yaml file with default content

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,15 +1,38 @@
 //This will have an init function which will currently create a .vault folder in the current directory
 // VAULT INIT
-use std::fs::create_dir;
+use std::fs::{self, create_dir};
 use std::path::Path;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct Init_Layout { 
+    current_branch: String, 
+    branches: Vec<String>,
+}
 
 pub fn init() {
     let vault_folder = Path::new(".vault");
-    if vault_folder.exists() {
-        println!("Vault already initialized");
-    } else {
-        create_dir(vault_folder).unwrap();
-    }
-    // @TODO: Add call to branch function with default(master) parameter.
-    // @TODO: Add call to switch function with default(master) parameter.
+//     if vault_folder.exists() {
+//         println!("Vault already initialized")
+//     }
+   match create_dir(vault_folder) {
+       Ok(_) => {
+           let init_file = vault_folder.join("init.yaml");
+           let content: String = get_init_data();
+           fs::write(init_file, content).unwrap();
+        // @TODO: Add call to branch function with default(master) parameter.
+        // @TODO: Add call to switch function with default(master) parameter.
+       },
+       Err(_) => println!("Vault already initialized")
+   }
+}
+
+fn get_init_data() -> String {
+   let yaml_struct = Init_Layout {
+       current_branch: "master".to_string(),
+       branches: vec!["master".to_string()],
+   };
+
+    let yaml_string = serde_yaml::to_string(&yaml_struct).unwrap();
+    return yaml_string;
 }
